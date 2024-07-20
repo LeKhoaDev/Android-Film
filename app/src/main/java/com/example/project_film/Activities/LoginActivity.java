6b8f2a856c0c1bc2b6f7a81347ed05397958e144
LeKhoaDev <lephamanhkhoa201203@gmail.com>
2024-07-20 21:12:34 +0700

    first commit

---
+++ app/src/main/java/com/example/project_film/Activities/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.project_film.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.project_film.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userEdt, passEdt;
+    private Button loginBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        initView();
+    }
+
+    private void initView() {
+        userEdt = findViewById(R.id.editTextText);
+        passEdt = findViewById(R.id.editTextPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            if (userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()) {
+                Toast.makeText(LoginActivity.this, "Please Fill your user and password", Toast.LENGTH_SHORT).show();
+            } else if (userEdt.getText().toString().equals("project") && passEdt.getText().toString().equals("test")) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            } else {
+                Toast.makeText(LoginActivity.this, "Your user and password isn't correct", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/project_film/Domain/Genres.java
@@ -0,0 +1,20 @@
+
+package com.example.project_film.Domain;
+
+
+import java.util.List;
+
+
+public class Genres {
+
+    private List<GenresItem> genres;
+
+    public List<GenresItem> getGenres() {
+
+        return genres;
+    }
+
+    public void setGenres(List<GenresItem> genres) {
+        this.genres = genres;
+    }
+}
